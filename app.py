a7747a3e74d54eb26cb5e3a4f261982db0f54665
Peristratus <testcorp24@gmail.com>
2021-02-22 06:05:19 +0000

    adding the form inputs, the routes for submission into Mongodb

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 
 @app.route("/")
 
-
+@app.route("/home")
 def home():
     return render_template('index.html')
 
@@ -32,6 +32,24 @@ def get_tasks():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        #check if existing username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "name": request.form.get("name").lower()
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into 'session' cookie
+        session["user"]= request.form.get("username").lower()
+        flash("Registration Succesfull")
     return render_template("register.html")
 
 
