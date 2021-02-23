03a4f93e60f4b02c54fce9a03765adc34a0b86b7
Peristratus <testcorp24@gmail.com>
2021-02-23 05:15:23 +0000

    bug in the add_task.html and /add_task route has resurfaced

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.dropdown-trigger').dropdown();
     $('.collapsible').collapsible();
-     $('.tooltipped').tooltip();
-     $('.datepicker').datepicker({
+    $('.tooltipped').tooltip();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
         format: "dd mmmm, yyyy",
         yearRange: [1930,2021],
         showClearBtn: true,
