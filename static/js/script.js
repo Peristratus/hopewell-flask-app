2e587884e9eb1ae907f6c2029751991d869dd828
Peristratus <testcorp24@gmail.com>
2021-02-23 04:08:23 +0000

    added task form input feilds

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,17 @@
-var currYear = (new Date()).getFullYear();
+
 
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.dropdown-trigger').dropdown();
     $('.collapsible').collapsible();
      $('.tooltipped').tooltip();
-    $(".datepicker").datepicker({
-    defaultDate: new Date(currYear-5,1,31),
-    // setDefaultDate: new Date(2000,01,31),
-    maxDate: new Date(currYear-5,12,31),
-    yearRange: [1928, currYear-5],
-    format: "yyyy/mm/dd"    
-      });
+     $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: [1930,2021],
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+
+     });
   });
